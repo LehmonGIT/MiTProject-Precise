a42042a88a591eb4e30d8e9a1a207fccb41b3041
LehmonGIT <Netchanokrotain@gmail.com>
2025-12-19 17:03:14 +0700

    fix database environment variable name

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
-import psycopg2
 import os
+import psycopg2
 
 def get_db():
-    return psycopg2.connect(
-        host=os.environ["DB_HOST"],
-        port=os.environ["DB_PORT"],
-        database=os.environ["DB_NAME"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"]
+    conn = psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASS"), 
+        port=os.environ.get("DB_PORT")
     )
-print(os.environ.get("DB_HOST"))
+    return conn
