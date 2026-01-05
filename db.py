d6d3b19cbb6438b11b902d7fbf18241b31f1ad67
LehmonGIT <Netchanokrotain@gmail.com>
2026-01-05 16:02:44 +0700

    feat: print error db

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 import os
 import psycopg2
 
+print("DATABASE_URL =", os.environ.get("DATABASE_URL"))
+
 def get_db():
     database_url = os.environ.get("Database_URL")
-
+    
     if not database_url:
         raise RuntimeError("DATABASE_URL not set")
 
