c590ee4ec5f0f61e3ee8b63810e3f3658a703d38
LehmonGIT <Netchanokrotain@gmail.com>
2026-01-06 11:20:41 +0700

    feat: show line error

--- app.py
+++ app.py
@@ -239,9 +239,13 @@ def delete_product(id):
 #         "DATABASE_URL": bool(os.environ.get("DATABASE_URL"))
 #     }
 
+    
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
     # app.run(debug=True)
